3ffe2b9f22aab92dc2032efd1015031810afb964
harish-sethuraman <ssharishkumar@gmail.com>
2023-10-18 13:40:19 +0530

    chore: add website build

---
+++ vite.website.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as packageJson from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  build: {
+    lib: {
+      name: "vercel-nav",
+      fileName: "index",
+      entry: "src/index.tsx",
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+});
